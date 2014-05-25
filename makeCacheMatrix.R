a0a6a3012e59bd965eb825ef8fff15f6fa9e9687
Pedro Bazzarella da Silva <pedro99@globo.com>
2014-05-25 09:35:56 -0300

    Update makeCacheMatrix.R

--- makeCacheMatrix.R
+++ makeCacheMatrix.R
@@ -1,13 +1,33 @@
-makeCacheMatrix <- function(x = numeric()) {
-        m <- NULL
+# Programming Assignment 2 
+#
+# DETAILS: This function creates a special "matrix" object that can cache its inverse
+# ARGUMENTS: M -> A matrix thats is null for default
+# RETURNS: A List with 4 functions (get(), set(), getInverse, setInverse()
+# AUTHOR: Pedro Bazzarella
+# DATE: 2014-05-25
+#---------------------------------------------------------------------------------
+#                               HISTORY
+# --------------------------------------------------------------------------------
+# VERSION  | DATE       | AUTHOR           | DESCRIPTION
+#---------------------------------------------------------------------------------
+#1.0       | 2014-05-25 | Pedro Bazzarella | Inicital Version
+#
+
+
+makeCacheMatrix <- function(m = matrix()) {
+        #create object mInverse and set it to null
+        mInverse <- NULL
         set <- function(y) {
-                x <<- y
-                m <<- NULL
+                m <<- y
+                mInverse <<- NULL
         }
+        
+        #definition of return functions
         get <- function() x
-        setmean <- function(mean) m <<- mean
-        getmean <- function() m
+        setInverse <- function(inverse) mInverse <<- inverse
+        getInverse <- function() mInverse
         list(set = set, get = get,
-             setmean = setmean,
-             getmean = getmean)
+             setInverse = setInverse,
+             getInverse = getInverse)
 }
+
